f7396fd9575639cfe9d6ebbdeb2916e1f07c610e
Vanya-Ch <mr.vanya882@gmail.com>
2024-02-13 17:00:09 +0200

    final

---
+++ assets/js/cart.js
@@ -0,0 +1,113 @@
+const cards = document.querySelectorAll('.main__card');
+const cartStorage = JSON.parse(localStorage.getItem('cart') || '[]');
+
+cards.forEach((el, idx) => {
+    let btnAdd = el.childNodes[7],
+        title = el.childNodes[3].innerText,
+        price = el.childNodes[5].innerText.slice(0, -1);
+        
+
+    btnAdd.addEventListener('click', () => {
+        const cartStorage = localStorage.getItem('cart') || '[]';
+        const cart = JSON.parse(cartStorage);
+        const card = { title, price };
+        localStorage.setItem('cart', JSON.stringify([...cart, card]))
+        location.reload()
+    });
+
+
+})
+
+let clearCart = document.createElement('button')
+clearCart.classList.add('cart__clear');
+clearCart.innerText = 'Очистити кошик'
+clearCart.addEventListener('click', () => {
+    localStorage.clear();
+    location.reload()
+})
+
+document.querySelector('.cart__footer').prepend(clearCart)
+
+function filterUniqueByTitle(cartStorage) {
+    const cart = [];
+    const encounteredTitles = new Set();
+
+    for (const obj of cartStorage) {
+        if (!encounteredTitles.has(obj.title)) {
+            encounteredTitles.add(obj.title);
+            cart.push(obj);
+        }
+    }
+
+    return cart;
+}
+
+const cart = filterUniqueByTitle(cartStorage);
+
+
+if (cart.length) {
+    cart.forEach((el) => {
+        let { title, price} = el;
+        let item = document.createElement('div'),
+            itemPrice = document.createElement('div'),
+            itemCount = document.createElement('div'),
+            itemTitle = document.createElement('div'),
+            itemTotalPrice = document.createElement('div'),
+            btnPlus = document.createElement('button'),
+            btnMinus = document.createElement('button'),
+            cartPrice = document.createElement('div');
+
+        let iCount = 1;
+
+        itemCount.innerText = iCount;
+        itemTitle.innerText = title;
+        itemTotalPrice.innerText = price * iCount + '₴';
+
+        itemPrice.innerText = price + '₴';
+
+
+
+
+        item.classList.add('item');
+        itemTitle.classList.add('item__title');
+        itemPrice.classList.add('item__price');
+        itemCount.classList.add('item__count');
+        btnPlus.innerText = '+';
+        btnMinus.innerText = '-';
+        btnMinus.classList.add('item__minus');
+        btnPlus.classList.add('item__plus');
+
+        btnPlus.addEventListener('click', () => {
+            if (iCount >= 0) {
+                iCount++;
+                itemCount.innerText = iCount;
+                updateValue()
+            }
+        });
+        btnMinus.addEventListener('click', () => {
+            if (iCount > 0) {
+                iCount--;
+                itemCount.innerText = iCount;
+                updateValue()
+            }
+        });
+
+
+        function updateValue() {
+            itemTotalPrice.innerText = iCount * price + '₴';
+            
+        }
+
+
+        item.append(itemTitle, itemPrice, btnPlus, itemCount, btnMinus, itemTotalPrice)
+
+        document.querySelector('.cart__main').append(item);
+        document.querySelector('.cart__totalPrice').append(cartPrice);
+    })
+    
+    
+    
+}
+
+
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,131 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.burger').addEventListener('click', (e) => {
-        e.target.classList.toggle('active');
-        document.querySelector('.navigation').classList.toggle('open')
-    })
+    let list = document.querySelector('.catalog-list__content');
+    let avto = `<li class="catalog-list__item">
+    <a class="catalog-list__link" href="./cars/engine/">
+        Автомобільні двигуни
+        <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+            <path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg>
+    </a>
+</li>
+<li class="catalog-list__item">
+    <a class="catalog-list__link" href="./cars/car body/">
+        Автомобільні кузови
+        <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+            <path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg>
+    </a>
+</li>
+<li class="catalog-list__item">
+    <a class="catalog-list__link" href="./cars/oil/">
+        Автомобільні масла
+        <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+            <path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg>
+    </a>
+</li>
+<li class="catalog-list__item">
+    <a class="catalog-list__link" href="./cars/filters/">
+        Автомобільні фільтри
+        <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+            <path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg>
+    </a>
+</li>`,
+track = `<li class="catalog-list__item">
+<a class="catalog-list__link" href="./tracks/engine/">
+    Вантажні двигуни
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+<li class="catalog-list__item">
+<a class="catalog-list__link" href="./tracks/track body/">
+    Вінтажні кузови
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+<li class="catalog-list__item">
+<a class="catalog-list__link" href="./tracks/oil/">
+    Вантажні масла
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+<li class="catalog-list__item">
+<a class="catalog-list__link" href="./tracks/filters/">
+    Вантажні фільтри
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>`,
+tire = `<li class="catalog-list__item">
+<a class="catalog-list__link" href="./tires/summer-tires/">
+    Літні шини
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+<li class="catalog-list__item">
+<a class="catalog-list__link" href="./tires/winter-tires/">
+    Зимові шини
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+`,
+accessory = `<li class="catalog-list__item">
+<a class="catalog-list__link" href="./accessories/air-fresheners/">
+    Освіжувачі повітря
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+<li class="catalog-list__item">
+<a class="catalog-list__link" href="./accessories/different/">
+    Аксесуари
+    <svg class="catalog-list__arrow" width="12.25" height="14" viewBox="0 0 448 512">
+        <path
+            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+    </svg>
+</a>
+</li>
+`
+
+
+
+document.querySelector('.car').addEventListener('click', ()=>{
+    list.innerHTML=avto;
+})
+document.querySelector('.autobus').addEventListener('click', ()=>{
+    list.innerHTML=track;
+})
+document.querySelector('.tire').addEventListener('click', ()=>{
+    list.innerHTML=tire;
+})
+document.querySelector('.accessory').addEventListener('click', ()=>{
+    list.innerHTML=accessory;
+})
+
+
+
 });
